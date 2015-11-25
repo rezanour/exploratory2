27617afc30ff2546cf29da647e4eb981e581b9e9
Reza Nourai <rezanour@hotmail.com>
2015-11-24 21:16:38 -0800

    Fix broken filmic operator.

--- HDR_ToneMapping/Graphics.cpp
+++ HDR_ToneMapping/Graphics.cpp
@@ -155,6 +155,8 @@ void GraphicsClear()
 {
     static const float clearColor[] = { 0, 0, 0, 1 };
     Context->ClearRenderTargetView(BackBufferRTV.Get(), clearColor);
+    Context->ClearRenderTargetView(HighPassRTV.Get(), clearColor);
+    Context->ClearRenderTargetView(BlurRTV.Get(), clearColor);
 }
 
 void GraphicsPresent()
